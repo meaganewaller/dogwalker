9db78ef3a0caec5819540acf017ace015b70cde7
Meagan Waller <meaganewaller@gmail.com>
2021-01-10 20:51:58 -0500

    ✨ Create locations yaml file and have ability to query it using graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,24 @@
 module.exports = {
     plugins: [
-
-    ]
-}
+        {
+            resolve: "gatsby-transformer-yaml",
+            options: {
+                typeName: ({node}) => {
+                    const name = node.sourceInstanceName
+                    if (name === `locations`) {
+                        return `Location`
+                    }
+                    return name
+                },
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: "./locations",
+                name: "locations",
+            },
+            __key: "locations",
+        },
+    ],
+};
